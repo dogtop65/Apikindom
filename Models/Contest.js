d944e72f01d27822dafe1fbadd4012ab0ceece74
dogtop65 <oparshadwarsi@gmail.com>
2025-08-04 09:55:45 +0530

    Update Contest.js

--- Models/Contest.js
+++ Models/Contest.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
 
+const ContestSchema = new mongoose.Schema({
+  matchId: String,
+  contests: [
+    {
+      name: String,
+      prize: String,
+      entry: String,
+      spots: String,
+      filled: String,
+      mega: Boolean,
+      guaranteed: Boolean,
+      bonus: String
+    }
+  ]
+});
+
+module.exports = mongoose.model('Contest', ContestSchema);
